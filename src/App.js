e2302ddff12c4aa53079f074297019ed4687d31f
Abhishek-gb <gbabhishek6361@gmail.com>
2024-11-10 12:11:16 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './components/Header'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Routing from './Routing/Routing'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
     <Header/>
     <Routing/>
+    <Footer/>
     </>
   )
 }
